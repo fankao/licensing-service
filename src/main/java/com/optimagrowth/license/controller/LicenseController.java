79f6dd8557dfa9e47f2bb150ed2787bf29073b73
chien <minhchiendc@gmail.com>
2021-06-16 20:32:39 +0700

    Implementing Spring HATEOAS to display related links. closes #1

--- src/main/java/com/optimagrowth/license/controller/LicenseController.java
+++ src/main/java/com/optimagrowth/license/controller/LicenseController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Locale;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 @RestController
 @RequestMapping("v1/organization/{organizationId}/license")
 public class LicenseController {
@@ -18,20 +21,30 @@ public class LicenseController {
     public ResponseEntity<License> getLicense(@PathVariable("organizationId") String organizationId,
                                               @PathVariable("licenseId") String licenseId) {
         License license = licenseService.getLicense(licenseId, organizationId);
+        license.add(linkTo(methodOn(LicenseController.class)
+                        .getLicense(organizationId, license.getLicenseId()))
+                        .withSelfRel(),
+                linkTo(methodOn(LicenseController.class)
+                        .createLicense(organizationId, license, null))
+                        .withRel("createLicense"),
+                linkTo(methodOn(LicenseController.class)
+                        .updateLicense(organizationId, license))
+                        .withRel("updateLicense"),
+                linkTo(methodOn(LicenseController.class)
+                        .deleteLicense(organizationId, license.getLicenseId()))
+                        .withRel("deleteLicense"));
         return ResponseEntity.ok(license);
     }
 
     @PutMapping
-    public ResponseEntity<String> updateLicense(@RequestBody License license,
-                                                @PathVariable("organizationId") String organizationId,
-                                                @RequestHeader(value = "Accept-Language",required = false)
-                                                            Locale locale) {
+    public ResponseEntity<String> updateLicense(@PathVariable("organizationId") String organizationId,
+                                                @RequestBody License license) {
         return ResponseEntity.ok(licenseService.updateLicense(license, organizationId));
     }
 
     @PostMapping
-    public ResponseEntity<String> createLicense(@RequestBody License license,
-                                                @PathVariable("organizationId") String organizationId,
+    public ResponseEntity<String> createLicense(@PathVariable("organizationId") String organizationId,
+                                                @RequestBody License license,
                                                 @RequestHeader(value = "Accept-Language",required = false)
                                                             Locale locale) {
         return ResponseEntity.ok(licenseService.createLicense(license, organizationId, locale));
